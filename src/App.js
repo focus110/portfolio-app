ad49614da6313957fd5c55983267d146e8b84866
focus110 <iamemmaochuko@gmail.com>
2022-08-22 18:50:36 +0100

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "./component/layout/Header";
+import Works from "./component/layout/Works";
+import Experience from "./component/layout/Experience";
+import Footer from "./component/layout/Footer";
 
 function App() {
-  return <div className="App">Hello word</div>;
+  return (
+    <div className="App">
+      <div>
+        <Header />
+        <Works />
+        <Experience />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./main/Home";
+import Work from "./main/Work";
+
+export { Home, Work };
