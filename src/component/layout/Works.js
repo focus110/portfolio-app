27bc46c21e07fae04e19fe7ccd34c79bb2c64ab7
focus110 <iamemmaochuko@gmail.com>
2022-08-22 20:51:11 +0100

    works ui component

--- src/component/layout/Works.js
+++ src/component/layout/Works.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Works = () => {
-  return <div>Works</div>;
+  const works = [
+    {
+      id: "1",
+      img: "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      title: "Contact Keeper App",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit Voluptatibus quia, Nonea! Maiores et perferendis eaque,exercitationem praesentium nihil.",
+      url: "/work",
+    },
+    {
+      id: "1",
+      img: "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      title: "Contact Keeper App",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit Voluptatibus quia, Nonea! Maiores et perferendis eaque,exercitationem praesentium nihil.",
+      url: "/work",
+    },
+  ];
+
+  return (
+    <div className="block space-y-14 text-fade-black py-1 md:py-20">
+      <div className="text-xl text-black">Some Concept Works</div>
+      {works.map((work, i) => {
+        const { id, img, title, desc, url } = work;
+        return (
+          <div className=" w-full lg:max-w-full lg:flex">
+            <div
+              className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+              style={{
+                backgroundImage: `url(${img}
+            )`,
+              }}
+              title="Mountain"
+            ></div>
+
+            <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+              <div className="mb-8">
+                <div className="block text-gray-900 font-bold text-xl mb-2">
+                  <Link to={url}> {title}</Link>
+                </div>
+                <p className="block text-gray-700 text-base md:w-3/5">
+                  <Link to={url}>{desc}</Link>
+                </p>
+              </div>
+              <div className="block items-center">
+                <p>
+                  <Link to={url}>More Info</Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Works;
--- src/pages/main/Home.js
+++ src/pages/main/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../component/layout/Header";
+import Works from "../../component/layout/Works";
 
 const Home = () => {
   return (
     <div className="container">
       <div>
         <Header />
+        <Works />
       </div>
     </div>
   );
